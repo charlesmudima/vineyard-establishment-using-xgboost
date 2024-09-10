63ddab0ad45a0272425b03d5292a72ee2d4ecc5c
Mudima, TC, Mr [23969156@sun.ac.za] <23969156@sun.ac.za>
2024-09-11 00:46:07 +0200

    online tutorial, model testing and practice

---
+++ xgboost-test.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
+
+diamonds = sns.load_dataset("diamonds")
+
+diamonds.head()
+
+diamonds.shape
